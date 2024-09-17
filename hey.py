c92c39e3125e856db37663a5a91877fe27abe407
jbt <linoffer@gmail.com>
2024-09-17 15:49:52 +0300

    Initial commit

---
+++ hey.py
@@ -0,0 +1,60 @@
+# Python program to create a table
+
+from tkinter import *
+import json
+
+class Table:
+
+    def __init__(self, root):
+
+        # code for creating table
+        for i in range(total_rows):
+            for j in range(total_columns):
+                self.e = Entry(root, width=20, fg='blue',
+                               font=('Arial', 16, 'bold'))
+
+                self.e.grid(row=i, column=j)
+                self.e.insert(END, lst[i][j])
+
+
+# take the data
+for i in range(3):
+    S = [["free" for x in range(6)] for i in range(6)]
+    file = open("sch.txt"+str(i), 'w')
+    json.dump(S,file)
+
+
+milon = {'pysics': 2, 'math': 3,"loop":5}
+
+
+x = 0
+for k in range(3):
+    file = open("sch.txt"+str(k), 'r')
+    S = json.load(file)
+    for i in milon:
+        for j in range(milon[i]):
+            for d in range(len(S)):
+                for h in range(len(S[d])):
+                    if S[d][h] == "free" and x < milon[i]:
+                        S[d][h] = i
+                        x += 1
+
+        x = 0
+    file = open("sch.txt" + str(k), 'w')
+    json.dump(S, file)
+
+for i in S:
+    print(i)
+lst = S
+
+    # find total number of rows and
+    # columns in list
+for i in range (3):
+    total_rows = len(lst)
+    total_columns = len(lst[0])
+
+    # create root window
+    root = Tk()
+    t = Table(root)
+root.mainloop()
+
